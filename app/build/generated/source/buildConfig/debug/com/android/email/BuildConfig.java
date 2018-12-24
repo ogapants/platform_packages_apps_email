6a1772b5a903eb95787037a9bdd4a9c977aa5484
ogapants <ogapamyu@gmail.com>
2018-12-24 16:41:17 +0900

    add modules

---
+++ app/build/generated/source/buildConfig/debug/com/android/email/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.android.email;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.android.email";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 7008110;
+  public static final String VERSION_NAME = "";
+}
